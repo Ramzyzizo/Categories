1e978f3518fc4e08c7b55e184bc5f971bc9a3322
Ramzy Saad <www.ramzysaad16@gmail.com>
2023-08-31 22:57:57 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/',CategoryController::class);
+Route::get('/sub_category/{category_id}',[CategoryController::class,'create_subcategory'])->name('sub_category');
